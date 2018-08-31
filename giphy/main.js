12a2d950c4254df10c5b978596fe0533fe6607d3
rmaharja@gmail.com <rmaharja@gmail.com>
2018-08-30 23:17:48 -0400

    some css

---
+++ giphy/main.js
@@ -0,0 +1,119 @@
+$(document).ready(function(){
+
+  
+  var reactionsArray= [ "wtf", "wow", "nope", "see ya", "OMG", "hahaha", "mindblown",];
+  
+  function createButtons (){
+  
+    // makes sure that the button does not repeat.
+    $("#buttonSection").empty();
+    //for loop to loop thru every giph
+  
+    for (var i=0; i< reactionsArray.length; i++){
+      // Creating a variable that is a button
+      var newButtons= $("<button>");
+      // Adding class to the newGiphBtn for easier selection of buttons.
+      newButtons.addClass("giphBtn");
+      // Adding a data-attribute to add extra info
+      newButtons.attr("data-reaction", reactionsArray[i]);
+      // Add the text to the button based on the selected array.
+      newButtons.text(reactionsArray[i]);
+      $("#buttonSection").append(newButtons);
+  
+  
+    }
+  
+  }
+  
+  //Creating new buttons
+  function createNewButtons(){
+    $("#submitButton").on("click", function (){
+    
+      event.preventDefault();
+    //Getting user input value from the textbox
+      var reaction= $("#searchText").val().trim();
+      // console.log(reaction);
+    //Getting the movies  based on the into reactionArray
+      reactionArray.push(reaction);
+      
+      // Create new Buttons
+      createButtons();
+    });
+    }
+  
+    function displayGiph(){
+      var reaction= $(this).attr("data-reaction");
+
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ reaction + "&api_key=dc6zaTOxFJmzC&limit=10&offset=10";
+      console.log(queryURL);
+
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      .then(function(response){
+        console.log(response);
+
+        $("#giphSection").empty() //clears previous giphs
+
+      
+      for (var i = 0; i<response.data.length; i++){
+
+        // Creating div/class so that the new giphs can go in it.
+        var giphDiv= $("<div>").addClass("giphClass");
+        
+        //Retreiving Rating and adding it to a <p> tag
+        var rated= response.data[i].rating;
+        var pRating= $("<p>").text("Giph Rating:"+ rated);
+        giphDiv.append(pRating);
+
+        //Retrieving Giph images (still, src and animated)
+        var srcURL= response.data[i].images.fixed_height_still.url;
+        var stillURL= response.data[i].images.fixed_height_still.url;
+        var animatedURL= response.data[i].images.fixed_height.url;
+
+        var giphImage= $("<img>").attr("src", srcURL);
+        giphImage.addClass("giphImages")
+        //Adding attr to the image and setting data state. 
+        giphImage.attr("data-still", stillURL );
+        giphImage.attr("data-animated", animatedURL );
+        giphImage.attr("data-state", "still");
+        //Adding the gif to the div
+        giphDiv.append(giphImage);
+        // Adding gifDiv to the HTML 
+        $("#giphSection").append(giphDiv);
+      }
+    });
+  }
+  //Making function do some work (calling them)
+
+  displayGiph();
+  createNewButtons();
+  createButtons();
+
+  //On click functions
+// Display giphs when clicking the buttons
+  $(document).on("click", ".giphBtn", displayGiph());
+
+//pausing and unpausing the images.
+$(document).on('click', '.giphImages', function(){
+  var state = $(this).attr("data-state");
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+
+});
+
+  
+  
+    
+    
+  
+  
+  
+    // End of document.ready
+  });
